e60a2d9a950524dd342c22109c0334555cd68cb4
michaldobiezynski <dobiezynski@gmail.com>
2020-05-15 15:48:45 +0100

    finishing the action buttons

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ new Vue({
       }
       this.monsterAttacks();
     },
-    giveUp: function () {},
+    giveUp: function () {
+      this.gameIsRunning = false;
+    },
     calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
